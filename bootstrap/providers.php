d400bab501266872987f8cbe93341e2222e4383c
Viktor Grishchenko <just.v.gm@gmail.com>
2025-06-09 22:52:58 +0200

    Add Telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Providers\AppServiceProvider;
+use App\Providers\TelescopeServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider as LibTelescopeServiceProvider;
+
+$localProviders = [];
+if (
+    app()->environment('local')
+    && class_exists(LibTelescopeServiceProvider::class)
+    && class_exists(TelescopeServiceProvider::class)
+) {
+    $localProviders[] = LibTelescopeServiceProvider::class;
+    $localProviders[] = TelescopeServiceProvider::class;
+}
+
 return [
-    App\Providers\AppServiceProvider::class,
+    AppServiceProvider::class,
+    ...$localProviders
 ];
